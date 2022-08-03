47072a907caea749706832618472bb15b6756e96
Devin <devster99@gmail.com>
2022-08-02 19:53:29 -0600

    added a whole bunch of new features

--- fooder.js
+++ fooder.js
@@ -1,1 +1,18 @@
 console.log('initial commit');
+
+const foods = ['asparagus', 'artichoke', 'cheese', 'cupcakes', 'honey', 'hot dogs'];
+const sensoryVerbs = ['looks', 'feels', 'smells', 'tastes'];
+const adjectives = ['bad', 'empty', 'air', '', 'colorful', 'dark', 'dull', 'filthy', 'pungent', 'magnificent', 'nasty', 'crusty', 'artificial', 'frigid', 'horizontal', 'safe', 'available', 'ilicit', 'spiritual'];
+
+const randomInteger = (arr) => {
+    let random = Math.floor(Math.random() * arr.length);
+    return random;
+}
+
+let randomFoodIndex = randomInteger(foods);
+let randomSensoryVerbIndex = randomInteger(sensoryVerbs);
+let randomAdjectiveIndex= randomInteger(adjectives);
+
+console.log(`${foods[randomFoodIndex]} ${sensoryVerbs[randomSensoryVerbIndex]} ${adjectives[randomAdjectiveIndex]}`);
+
+export default test = () => 'hello world'
